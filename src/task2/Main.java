3cd686b25d29a24121c45a9e83ed5e14be0ef244
Galina <krasnikova.82@mail.ru>
2020-12-21 00:26:14 +0300

    ДЗ 8

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,4 +1,84 @@
 package task2;
 
-public class Main {
+//    2. Написать интерфейсы Fly, Run и Swim чтобы в каждом было по одному методу.
+//    Добавить классам животных из предыдущего задания имплементацию этих интерфейсов.
+//    Некоторые животные могут реализовать больше одного интерфейса (утка может и плавать, и летать и бегать).
+
+
+abstract class Animal { // абстрактный класс
+    String NameAnimal;
+
+    abstract String getName ();//абстрактный метод
 }
+class Herbivores extends Animal implements  Run, Swim {
+
+    String Run;
+    String Swim;
+
+    public String getRun () {
+        return Run;
+    }
+    public String getSwim () {
+        return Swim;
+    }
+
+    public String getName () {
+        return NameAnimal;
+    }
+
+    Herbivores (String name,  String R, String S){ // конструктор
+        NameAnimal =name;
+        Run = R;
+        Swim = S;
+    }
+}
+class Predators extends Animal implements Fly, Run, Swim{
+    String Fly;
+    String Run;
+    String Swim;
+
+    public String getName () {
+        return NameAnimal;
+    }
+    public String getFly () {
+        return Fly;
+    }
+    public String getRun () {
+        return Run;
+    }
+    public String getSwim () {
+        return Swim;
+    }
+    Predators (String name, String F, String R, String S){ // конструктор
+        NameAnimal =name;
+        Fly = F;
+        Run = R;
+        Swim = S;
+
+    }
+
+}
+interface Fly {
+    abstract String getFly ();
+}
+interface Run {
+     abstract String getRun ();
+}
+interface Swim {
+     abstract String getSwim ();
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Herbivores Herbiv = new Herbivores("Бегемот","бегать", "плавать");
+        Predators Preda = new Predators("Аист", "летать","бегаеть","плавать");
+        System.out.println("Травоядное животное -" +Herbiv.getName()+ ", умеет: "+Herbiv.getRun()+", "+Herbiv.getSwim()+".");
+        System.out.println("Хищное животное -" +Preda.getName()+ ", умеет: "+Preda.getFly()+ ", "+Preda.getRun()+", "+Preda.getSwim()+".");
+    }
+}
+
+
+
+
--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,4 +1,88 @@
 package task3;
+//   3. Написать абстрактный класс Человек реализующий интерфейсы «бежать» и «плавать»
+//   (в каждом сделать 1-2 метода). Сделать несколько наследников этого класса с конкретной реализацией методов,
+//   которые объявлены в интерфейсах.
+
+
+
+abstract class Person implements Run, Swim {// абстрактный касс
+         abstract String getName ();// метод
+}
+
+interface Run {  // интерфейс1
+    abstract String getRun ();
+    abstract String getShoes ();
+}
+interface Swim {  // интерфейс2
+    abstract String getSwim ();
+}
+
+class Male extends Person { //класс мужчины
+    String Name;
+    String Run;
+    String Swim;
+    String Shoes;
+
+    public String getName () { // метод получения имени
+        return Name;
+    }
+
+    Male (String name,  String R, String S, String H){ // конструктор
+        Name =name;
+        Run = R;
+        Swim = S;
+        Shoes = H;
+    }
+
+    public String getRun () {
+        return Run;
+    }
+    public String getSwim () {
+        return Swim;
+    }
+    public String getShoes () {
+        return Shoes;
+    }
+}
+class Woman extends Person {
+    String Name;
+    String Run;
+    String Swim;
+    String Shoes;
+
+    public String getName () {
+        return Name;
+    }
+
+    Woman (String name,  String R, String S, String H){ // конструктор
+        Name =name;
+        Run = R;
+        Swim = S;
+        Shoes = H;
+    }
+
+    public String getRun () {
+      if (Shoes.startsWith("туф")) Run ="не бежит";
+      return Run;
+    }
+    public String getSwim () {
+        return Swim;
+    }
+    public String getShoes () {
+        return Shoes;
+
+    }
+}
+
+
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Male Male1 = new Male("Саша","бегает", "умеет плавать","кедах");
+        Woman Woman1 = new Woman("Инесса", "бежит","умеет плавать","туфлях");
+        System.out.println(Male1.getName()+ ", "+Male1.getRun()+" в "+Male1.getShoes()+", "+Male1.getSwim()+".");
+        System.out.println(Woman1.getName()+ ", "+Woman1.getRun()+" в "+Woman1.getShoes()+", "+Woman1.getSwim()+".");
+    }
 }
